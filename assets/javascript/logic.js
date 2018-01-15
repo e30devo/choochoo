bd6113748f3931a8b0e687813203d201ebf5f85c
e30devo <dthomase30@gmail.com>
2018-01-15 01:24:19 -0800

    Some functionality push.
    Storing data to Firebase.
    Dynamically creating table row/content pulling data on submit.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,6 @@
-// Initialize Firebase
-var config = {
+$(document).ready(function() {
+  // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyC24JVbFxBGtc_PorQNnDOhCE0gbM9-uZE",
     authDomain: "fir-101-9c773.firebaseapp.com",
     databaseURL: "https://fir-101-9c773.firebaseio.com",
@@ -9,35 +10,42 @@ var config = {
   };
   firebase.initializeApp(config);
   var database = firebase.database();
-  
 
+  //   database.ref().on("value", function(snapshot) {
+  //     console.log(snapshot.val());
+  //     });
 
   $(".submit").on("click", function() {
-         database.ref().on("value", function(snapshot) {           
-            console.log(snapshot.val());
-        });
-
-    var name = $("[name='name']").val().trim();
-    var destination = $("[name='destination']").val().trim();
-    var firstTime = $("[name='firstTime']").val().trim();
-    var frequency = $("[name='frequency']").val().trim();
+    var name = $("[name='name']")
+      .val()
+      .trim();
+    var destination = $("[name='destination']")
+      .val()
+      .trim();
+    var firstTime = $("[name='firstTime']")
+      .val()
+      .trim();
+    var frequency = $("[name='frequency']")
+      .val()
+      .trim();
 
     database.ref().push({
-            _name: name,
-            destination: destination,
-            firstTime: firstTime,
-            frequency: frequency        
-        });
-    
-    database.ref().on("child_added", function (data){
-        console.log(data.key.child("destination"));
+      _trainName: name,
+      destination: destination,
+      firstTime: firstTime,
+      frequency: frequency
     });
-    
 
+    database.ref().once("child_added", function(data) {
+      console.log(data.val()._trainName);
+
+      $(".trainble").append(
+        "<tr>",
+        "<th>" + data.val()._trainName + "</th>",
+        "<th>" + data.val().destination + "</th>",        
+        "<th>" + data.val().frequency + "</th>",            
+        "</tr>"
+      );
     });
-  
-
-
-
-
-
+  }); //submit.on("click") close
+}); //doc.on("ready") close
