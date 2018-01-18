14ff4fe42713c1a058f3ab4b4429a1284c5ab618
e30devo <dthomase30@gmail.com>
2018-01-17 21:51:52 -0800

    Added in moment.js functionality

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,73 +12,90 @@ $(document).ready(function() {
   var database = firebase.database();
 
   database.ref().once("value", function(snapshot) {
-    console.log(snapshot.val());
-    console.log(Object.keys(snapshot.val()));
-    console.log(snapshot.numChildren());
     var instanceKey = Object.keys(snapshot.val());
 
-    // database.ref().once("child_added", function(data) {
-    //   console.log(Object.keys(data.val()));
-    //   console.log(data.val()._trainName);
-
-    // $.each(snapshot, function( key, value ) {
-    //   console.log( key + ": " + value );
-    // });
-
     for (var i = 0; i < snapshot.numChildren(); i++) {
-      console.log(snapshot.val()[instanceKey[i]]["_trainName"]);
+      var tFrequency = snapshot.val()[instanceKey[i]]["frequency"];
+
+      var firstTime = snapshot.val()[instanceKey[i]]["firstTime"];
+
+      var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+
+      var currentTime = moment();
+
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+      var tRemainder = diffTime % tFrequency;
+
+      var tMinutesTillTrain = tFrequency - tRemainder;
+
+      var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
       $(".trainble").append(
         "<tr>",
         "<th>" + snapshot.val()[instanceKey[i]]["_trainName"] + "</th>",
         "<th>" + snapshot.val()[instanceKey[i]]["destination"] + "</th>",
         "<th>" + snapshot.val()[instanceKey[i]]["frequency"] + "</th>",
+        "<th>" + moment(nextTrain).format("hh:mm") + "</th>",
+        "<th>" + tMinutesTillTrain + "</th>",
         "</tr>"
       );
     } //for loop close
-    // });
   }); //database.ref on"value" closer
 
-  //this works, dont touch below here. #twss
   $(".submit").on("click", function() {
-    var name = $("[name='name']")
-      .val()
-      .trim();
-    var destination = $("[name='destination']")
-      .val()
-      .trim();
-    var firstTime = $("[name='firstTime']")
-      .val()
-      .trim();
-    var frequency = $("[name='frequency']")
-      .val()
-      .trim();
+    database.ref().once("value", function(snapshot) {
+      var instanceKey = Object.keys(snapshot.val());
 
-    database.ref().push({
-      _trainName: name,
-      destination: destination,
-      firstTime: firstTime,
-      frequency: frequency
+      for (var i = 0; i < snapshot.numChildren(); i++) {
+        var tFrequency = snapshot.val()[instanceKey[i]]["frequency"];
+
+        var firstTime = snapshot.val()[instanceKey[i]]["firstTime"];
+      }
+
+      var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+
+      var currentTime = moment();
+
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+      var tRemainder = diffTime % tFrequency;
+
+      var tMinutesTillTrain = tFrequency - tRemainder;
+
+      var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+      var name = $("[name='name']")
+        .val()
+        .trim();
+      var destination = $("[name='destination']")
+        .val()
+        .trim();
+      var firstTime = $("[name='firstTime']")
+        .val()
+        .trim();
+      var frequency = $("[name='frequency']")
+        .val()
+        .trim();
+
+      database.ref().push({
+        _trainName: name,
+        destination: destination,
+        firstTime: firstTime,
+        frequency: frequency
+      });
+
+      $(".trainble").append(
+        "<tr>",
+        "<th>" + name + "</th>",
+        "<th>" + destination + "</th>",
+        "<th>" + frequency + "</th>",
+        "<th>" + moment(nextTrain).format("hh:mm") + "</th>",
+        "<th>" + tMinutesTillTrain + "</th>",
+        "</tr>"
+      );
+
+      $("input").val("");
     });
-
-    $(".trainble").append(
-      "<tr>",
-      "<th>" + name + "</th>",
-      "<th>" + destination + "</th>",
-      "<th>" + frequency + "</th>",
-      "</tr>"
-    );
-
-    $("input").val("");
-    // database.ref().once("child_added", function(data) {
-    //   $(".trainble").append(
-    //     "<tr>",
-    //     "<th>" + data.val()._trainName + "</th>",
-    //     "<th>" + data.val().destination + "</th>",
-    //     "<th>" + data.val().frequency + "</th>",
-    //     "</tr>"
-    //   );
-    // });
   }); //submit.on("click") close
 }); //doc.on("ready") close
-2;
