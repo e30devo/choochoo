654728aa9bef89b8651b8b58750f0ba0115d73f5
e30devo <dthomase30@gmail.com>
2018-01-17 19:30:14 -0800

    Push/pull from firebase functionality

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -11,10 +11,35 @@ $(document).ready(function() {
   firebase.initializeApp(config);
   var database = firebase.database();
 
-  //   database.ref().on("value", function(snapshot) {
-  //     console.log(snapshot.val());
-  //     });
+  database.ref().once("value", function(snapshot) {
+    console.log(snapshot.val());
+    console.log(Object.keys(snapshot.val()));
+    console.log(snapshot.numChildren());
+    var instanceKey = Object.keys(snapshot.val());
 
+    // database.ref().once("child_added", function(data) {
+    //   console.log(Object.keys(data.val()));
+    //   console.log(data.val()._trainName);
+
+    // $.each(snapshot, function( key, value ) {
+    //   console.log( key + ": " + value );
+    // });
+
+    for (var i = 0; i < snapshot.numChildren(); i++) {
+      console.log(snapshot.val()[instanceKey[i]]["_trainName"]);
+
+      $(".trainble").append(
+        "<tr>",
+        "<th>" + snapshot.val()[instanceKey[i]]["_trainName"] + "</th>",
+        "<th>" + snapshot.val()[instanceKey[i]]["destination"] + "</th>",
+        "<th>" + snapshot.val()[instanceKey[i]]["frequency"] + "</th>",
+        "</tr>"
+      );
+    } //for loop close
+    // });
+  }); //database.ref on"value" closer
+
+  //this works, dont touch below here. #twss
   $(".submit").on("click", function() {
     var name = $("[name='name']")
       .val()
@@ -36,16 +61,24 @@ $(document).ready(function() {
       frequency: frequency
     });
 
-    database.ref().once("child_added", function(data) {
-      console.log(data.val()._trainName);
+    $(".trainble").append(
+      "<tr>",
+      "<th>" + name + "</th>",
+      "<th>" + destination + "</th>",
+      "<th>" + frequency + "</th>",
+      "</tr>"
+    );
 
-      $(".trainble").append(
-        "<tr>",
-        "<th>" + data.val()._trainName + "</th>",
-        "<th>" + data.val().destination + "</th>",        
-        "<th>" + data.val().frequency + "</th>",            
-        "</tr>"
-      );
-    });
+    $("input").val("");
+    // database.ref().once("child_added", function(data) {
+    //   $(".trainble").append(
+    //     "<tr>",
+    //     "<th>" + data.val()._trainName + "</th>",
+    //     "<th>" + data.val().destination + "</th>",
+    //     "<th>" + data.val().frequency + "</th>",
+    //     "</tr>"
+    //   );
+    // });
   }); //submit.on("click") close
 }); //doc.on("ready") close
+2;
